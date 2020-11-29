e0aa792f66c8097ad1f0ccc722b63c943eb0f696
dialogosdeplaton <dialogosdeplaton@gmail.com>
2020-11-29 01:59:06 -0300

    Primer Conexion con la base de datos

--- optoclinica/settings.py
+++ optoclinica/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path, os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent #originalDelProyecto
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'optoclinica.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3', #originalDelProyecto
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
     }
 }
